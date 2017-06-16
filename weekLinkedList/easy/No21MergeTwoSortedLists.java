58f2d4e1cb056b68875be04b490064977b516758
weekendooo <394686513@qq.com>
2017-06-16 13:50:26 -0700

    Add files via upload

--- weekLinkedList/easy/No21MergeTwoSortedLists.java
+++ weekLinkedList/easy/No21MergeTwoSortedLists.java
@@ -1,7 +1,7 @@
 package LinkedListEasy;
 
 public class No21MergeTwoSortedLists {
-
+	// recursive
 	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) return l2;
         if (l2 == null) return l1;
@@ -16,6 +16,41 @@ public class No21MergeTwoSortedLists {
             node.next = mergeTwoLists(l1, l2.next);
         }
         return node;
+    }
+	
+	
+	// iterative
+	public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        
+        ListNode cur = new ListNode(0);
+        ListNode beforeHead = cur;
+        
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                cur = cur.next;
+                l1 = l1.next;
+            }
+            else {
+                cur.next = l2;
+                cur = cur.next;
+                l2 = l2.next;
+            }
+        }
+        
+        if (l1 != null) {
+            cur.next = l1;
+            cur = cur.next;
+            l1 = l1.next;
+        }
+        if (l2 != null) {
+            cur.next = l2;
+            cur = cur.next;
+            l2 = l2.next;
+        }
+        return beforeHead.next;
     }
 	
 	public static void main(String[] args) {
