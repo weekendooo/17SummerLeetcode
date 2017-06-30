f190ffc33c9894415eac78d181c7c256bad0c730
weekendooo <394686513@qq.com>
2017-06-29 18:31:44 -0700

    Add files via upload

--- week6DP/No10RegularExpressionMatching.java
+++ week6DP/No10RegularExpressionMatching.java
@@ -3,7 +3,9 @@ package DPHard;
 public class No10RegularExpressionMatching {
 
 	public boolean isMatch(String s, String p) {
-        if (s == null || p == null) return false;
+        if (s == null || p == null) {
+        	return false;
+        }
         boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
         dp[0][0] = true;
         
@@ -15,16 +17,16 @@ public class No10RegularExpressionMatching {
         for (int i = 0 ; i < s.length(); i++) {
             for (int j = 0; j < p.length(); j++) {
                 if (p.charAt(j) == '.') {
-                    dp[i+1][j+1] = dp[i][j];
+                    dp[i + 1][j + 1] = dp[i][j];
                 }
                 if (p.charAt(j) == s.charAt(i)) {
-                    dp[i+1][j+1] = dp[i][j];
+                    dp[i + 1][j + 1] = dp[i][j];
                 }
                 if (p.charAt(j) == '*') {
-                    if (p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '.') {
-                        dp[i+1][j+1] = dp[i+1][j-1];
+                    if (p.charAt(j - 1) != s.charAt(i) && p.charAt(j - 1) != '.') {
+                        dp[i + 1][j + 1] = dp[i + 1][j - 1];
                     } else {
-                        dp[i+1][j+1] = (dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1]);
+                        dp[i + 1][j + 1] = (dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1]);
                     }
                 }
             }
